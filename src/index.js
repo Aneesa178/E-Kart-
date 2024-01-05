daa83909188402bf467056bc13e5920adb672707
Aneesa178 <aneesaashraf005@gmail.com>
2024-01-05 10:48:33 -0800

    E-Kart using redux

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import './bootstrap.min.css'
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}> {/* universally store can be used by defining it inside the provider*/ }
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
